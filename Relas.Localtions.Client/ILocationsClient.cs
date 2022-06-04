d12f539b057501342673606417b7f22bdc5b1b3d
S.Kalantyr <kalantyr@yandex.ru>
2022-06-04 13:20:17 +0300

    1

---
+++ Relas.Localtions.Client/ILocationsClient.cs
@@ -0,0 +1,11 @@
+﻿using Relax.Locations.Models;
+
+namespace Relas.Localtions.Client
+{
+    public interface ILocationsClient
+    {
+        Task<IReadOnlyCollection<LocationInfo>> GetAllLocationsAsync(string userToken, CancellationToken cancellation);
+
+        Task<IReadOnlyCollection<object>> GetStaticObjectsAsync(uint locationId, string userToken, CancellationToken cancellation);
+    }
+}
---
+++ Relax.Locations.Models/LocationInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Relax.Locations.Models
+{
+    public class LocationInfo
+    {
+        public uint Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
